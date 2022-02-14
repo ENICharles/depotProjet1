94bcd06961d46cbcfa86405c7b1495a310d8299f
Corentin Longein <corentin.longein2021@campus-eni.fr>
2022-02-14 15:09:05 +0100

    ajout sysout test test test dans accueilServlet

--- src/main/java/fr/eni/testGit/AccueilServlet.java
+++ src/main/java/fr/eni/testGit/AccueilServlet.java
@@ -25,7 +25,7 @@ public class AccueilServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		System.out.println("test test test");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
